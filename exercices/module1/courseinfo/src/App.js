4fbabe6d9eab205cc79505f9b85c71fb5c827eaf
loic_hernaut <loic.hernaut@student.vinci.be>
2022-09-18 16:08:24 +0200

    1.1-1.2 : refactor - modules

--- exercices/module1/courseinfo/src/App.js
+++ exercices/module1/courseinfo/src/App.js
@@ -1,36 +1,6 @@
-const Header = (props) => {
-  return (
-    <>
-      <h1>{props.course}</h1>
-    </>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.part1} exercise={props.exercises1} />
-      <Part part={props.part2} exercise={props.exercises2} />
-      <Part part={props.part3} exercise={props.exercises3} />
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <>
-      <p>{props.part} {props.exercise}</p>
-    </>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <>
-      <p>Number of exercises {props.total}</p>
-    </>
-  )
-}
+import Total from "./components/Total"
+import Header from "./components/Header"
+import Content from "./components/Content"
 
 const App = () => {
   const course = 'Half Stack application development'
