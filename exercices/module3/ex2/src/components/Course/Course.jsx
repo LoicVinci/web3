c1594747771ea6cad3811fb0b7fe5422253613f5
LoicVinci <loic.hernaut@student.vinci.be>
2022-10-20 22:29:05 +0200

    module4 Ex2.4

--- exercices/module3/ex2/src/components/Course/Course.jsx
+++ exercices/module3/ex2/src/components/Course/Course.jsx
@@ -1,14 +1,21 @@
 import Content from "../Content/Content"
 import Header from "../Header/Header"
+import Total from "../Total/Total"
 
 const Course = (props) => {
 
     const { course } = props
 
+    let total = 0;
+    course.parts.map(part=>
+        total+=part.exercices)
+    //TODO 2.2 Ajouter total
+
     return (
         <div>
         <Header course={course.name} />
         <Content parts={course.parts} />
+        <Total total={total} />
         </div>
     )
 }
