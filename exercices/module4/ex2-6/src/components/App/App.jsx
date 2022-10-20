f7678bd1217c160bf02a88b9f15606cd6f96ca4c
LoicVinci <loic.hernaut@student.vinci.be>
2022-10-20 22:54:26 +0200

    Module4 ex2.8

--- exercices/module4/ex2-6/src/components/App/App.jsx
+++ exercices/module4/ex2-6/src/components/App/App.jsx
@@ -3,29 +3,46 @@ import { useState } from 'react'
 const App = () => {
   const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
 
-  const addName = (event) => {
+  const handlePhoneChange = (event) => {
+    setNewPhone(event.target.value)
+  }
+
+  const addPerson = (event) => {
     event.preventDefault()
+    
+
+    persons.forEach(person => {if(person.name === newName) {
+      alert(`${newName} is already added to phonebook`)
+    }});
+    
     const personObject = {
-      name: newName
+      name: newName,
+      phone: newPhone
     }
   
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewPhone('')
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addPerson}>
         <div>
           name: <input 
                   value={newName}
                   onChange={handleNameChange}
+                />
+          phone: <input 
+                  value={newPhone}
+                  onChange={handlePhoneChange}
                 />
         </div>
         <div>
@@ -35,7 +52,7 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {persons.map(person => 
-          <li>{person.name}</li>
+          <li>{person.name} {person.phone}</li>
         )}
       </ul>
     </div>
