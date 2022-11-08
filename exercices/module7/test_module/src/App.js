8f562af61571d565eb68d79015efecdc03ffebdd
LoicVinci <loic.hernaut@student.vinci.be>
2022-11-08 12:57:32 +0100

    module7 Ex7.2

---
+++ exercices/module7/test_module/src/App.js
@@ -0,0 +1,36 @@
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from "react-router-dom"
+import Home from "./Home"
+import Users from "./Users"
+import Notes from "./Notes"
+
+const App = () => {
+
+  const padding = {
+    padding: 5
+  }
+
+  return (
+    <Router>
+      <div>
+        <Link style={padding} to="/">home</Link>
+        <Link style={padding} to="/notes">notes</Link>
+        <Link style={padding} to="/users">users</Link>
+      </div>
+
+      <Routes>
+        <Route path="/notes" element={<Notes />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+
+      <div>
+        <i>Note app, Department of Computer Science 2022</i>
+      </div>
+    </Router>
+  )
+}
+
+export default App
---
+++ exercices/module7/test_module/src/Notes.jsx
@@ -0,0 +1,19 @@
+import {
+    BrowserRouter as Router,
+    Routes, Route, Link
+  } from "react-router-dom"
+
+const Notes = ({notes}) => (
+    <div>
+      <h2>Notes</h2>
+      <ul>
+        {notes.map(note =>
+          <li key={note.id}>
+            <Link to={`/notes/${note.id}`}>{note.content}</Link>
+          </li>
+        )}
+      </ul>
+    </div>
+  )
+
+export default Notes
